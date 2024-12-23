36043fb0d8cc151d330737483d5560aa6ef3fa0b
nadia <nadiaelqadi0660@gmail.com>
2024-12-23 02:21:12 +0000

    commit

---
+++ src/main/java/com/gl/parcauto/dto/response/TechnicalVisitDtoResponse.java
@@ -0,0 +1,13 @@
+package com.gl.parcauto.dto.response;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * A DTO for the {@link com.gl.parcauto.entity.TechnicalVisit} entity
+ */
+public record TechnicalVisitDtoResponse(
+        Long id,
+        String vehicleLicensePlate,
+        LocalDate visitDate) implements Serializable {
+}
---
+++ src/main/java/com/gl/parcauto/mapper/TechnicalVisitMapper.java
@@ -0,0 +1,19 @@
+package com.gl.parcauto.mapper;
+
+import com.gl.parcauto.dto.request.TechnicalVisitDtoRequest;
+import com.gl.parcauto.dto.response.TechnicalVisitDtoResponse;
+import com.gl.parcauto.entity.TechnicalVisit;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface TechnicalVisitMapper {
+    TechnicalVisitMapper INSTANCE = Mappers.getMapper(TechnicalVisitMapper.class);
+    TechnicalVisit visitDtoRequestToTechnicalVisit(TechnicalVisitDtoRequest visitDtoRequest);
+    @Mappings({
+            @Mapping(target = "vehicleLicensePlate", source = "technicalVisit.vehicle_technical_visit.licensePlate"),
+    })
+    TechnicalVisitDtoResponse visitToTechnicalVisitDtoResponse(TechnicalVisit technicalVisit);
+}
